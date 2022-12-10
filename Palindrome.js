435224a11ec4f8f8875b705e896c57c8a6226e8f
unknown <md3530546@gmail.com>
2022-12-10 21:10:10 +0530

    created files of prime and palindrome

---
+++ Palindrome.js
@@ -0,0 +1,9 @@
+let str = "madam"
+
+for(let i=0; i>str.length; i++){
+
+bag+= str[i]
+
+}
+
+console.log(bag)
---
+++ Prime.js
@@ -0,0 +1,13 @@
+
+let num = 15
+
+for(let i=0; i<=num; i++){
+
+if(num %i =0 ) {
+
+count++
+}
+}
+
+if(count==2){
+console.log("yes it is prime)
